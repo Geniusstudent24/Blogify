4774abf1c5dc76f8317314bd957cb66e65005b95
Meet Nimavat <meetnimavat725@gmail.com>
2025-08-13 16:52:10 +0530

    changes....

--- public/main.js
+++ public/main.js
@@ -1,49 +1,49 @@
-// async function subscribe() {
-//   if (!("serviceWorker" in navigator) || !("PushManager" in window)) {
-//     alert("Push notifications are not supported by your browser.");
-//     return;
-//   }
+async function subscribe() {
+  if (!("serviceWorker" in navigator) || !("PushManager" in window)) {
+    alert("Push notifications are not supported by your browser.");
+    return;
+  }
 
-//   try {
-//     const registration = await navigator.serviceWorker.register("/sw.js");
-//     console.log("Service Worker registered successfully.");
+  try {
+    const registration = await navigator.serviceWorker.register("/sw.js");
+    console.log("Service Worker registered successfully.");
 
-//     const permission = await window.Notification.requestPermission();
-//     if (permission !== "granted") {
-//       throw new Error("Permission not granted for Notification");
-//     }
+    const permission = await window.Notification.requestPermission();
+    if (permission !== "granted") {
+      throw new Error("Permission not granted for Notification");
+    }
 
-//     const response = await fetch("/notifications/vapidPublicKey");
-//     const vapidPublicKey = await response.text();
+    const response = await fetch("/notifications/vapidPublicKey");
+    const vapidPublicKey = await response.text();
 
-//     const subscription = await registration.pushManager.subscribe({
-//       userVisibleOnly: true,
-//       applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
-//     });
-//     console.log("User subscribed successfully.");
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+    });
+    console.log("User subscribed successfully.");
 
-//     await fetch("/notifications/subscribe", {
-//       method: "POST",
-//       body: JSON.stringify(subscription),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
+    await fetch("/notifications/subscribe", {
+      method: "POST",
+      body: JSON.stringify(subscription),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-//     alert("You have successfully subscribed to notifications!");
-//   } catch (error) {
-//     console.error("Failed to subscribe the user: ", error);
-//     alert("Failed to subscribe. Please try again.");
-//   }
-// }
+    alert("You have successfully subscribed to notifications!");
+  } catch (error) {
+    console.error("Failed to subscribe the user: ", error);
+    alert("Failed to subscribe. Please try again.");
+  }
+}
 
-// function urlBase64ToUint8Array(base64String) {
-//   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-//   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-//   const rawData = window.atob(base64);
-//   const outputArray = new Uint8Array(rawData.length);
-//   for (let i = 0; i < rawData.length; ++i) {
-//     outputArray[i] = rawData.charCodeAt(i);
-//   }
-//   return outputArray;
-// }
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
