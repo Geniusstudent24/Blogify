a18058525e877fe94534d58be05c00750d83ef35
Meet Nimavat <meetnimavat725@gmail.com>
2025-08-13 22:47:59 +0530

    changes...

--- router/blog.js
+++ router/blog.js
@@ -15,12 +15,6 @@ function setIo(io) {
   ioInstance = io;
 }
 
-// webPush.setVapidDetails(
-//   "mailto:kingjunagadh737@gmail.com",
-//   process.env.VAPID_PUBLIC_KEY,
-//   process.env.VAPID_PRIVATE_KEY
-// );
-
 const s3BucketName = process.env.S3_BUCKET_NAME;
 
 const uploads = multer({
@@ -138,23 +132,6 @@ router.post(
         coverImage: req.file.location,
         category,
       });
-
-      // const payload = JSON.stringify({
-      //   title: `New Blog Post: ${title}`,
-      //   body: `A new post has been added in the ${category} category. Click to view!`,
-      // });
-
-      // const subscriptions = await Subscription.find({});
-
-      // for (const sub of subscriptions) {
-      //   try {
-      //     await webPush.sendNotification(sub, payload);
-      //   } catch (error) {
-      //     console.error("Error sending notification to a subscriber: ", error);
-      //   }
-      // }
-
-      // console.log(`Notification sent to ${subscriptions.length} subscribers.`);
 
       ioInstance.emit("new-blog", {
         title: Blog.title,
